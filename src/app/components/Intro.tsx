927ac857d7aec2f4e40919fe4da003a32f5d64e9
cooolman2008 <cooolman2008@gmail.com>
2024-07-15 23:16:25 +1000

    fix: avoid rough javascript

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -47,10 +47,10 @@ export default function Intro() {
 					</p>
 					<div className=" w-full flex justify-center ups">
 						<div className="flex flex-wrap gap-4">
-							<a href="javascript:void(0)" className="mx-auto flex justify-center">
+							<a href="#" onClick={(e) => e.preventDefault()} className="mx-auto flex justify-center">
 								<button className="safu-button-pops">Buy NFTs</button>
 							</a>
-							<a href="javascript:void(0)" className="mx-auto flex justify-center">
+							<a href="#" onClick={(e) => e.preventDefault()} className="mx-auto flex justify-center">
 								<button className="safu-button-primary">Buy $SAFU</button>
 							</a>
 						</div>
