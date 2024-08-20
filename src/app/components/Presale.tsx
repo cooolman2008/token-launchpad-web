348a138438e9eb653675f49d341060f61aaf49d0
cooolman2008 <cooolman2008@gmail.com>
2024-08-21 04:46:08 +1000

    feat: update presale link

--- src/app/components/Presale.tsx
+++ src/app/components/Presale.tsx
@@ -15,9 +15,13 @@ function Presale() {
 				<div className="w-full flex justify-center text-center py-6 md:py-8 bg-[#000000] border-t border-neutral-800 z-30 fixed bottom-0">
 					<div className="container flex max-md:flex-col justify-center">
 						<p className="text-4xl text-slate-200 font-normal italic max-md:mb-4">
-							<b className="text-4xl font-black">PRESALE</b> is on!
+							<b className="text-4xl font-black mr-2">PRESALE</b> is on!
 						</p>
-						<a href="https://app.safulauncher.com/" target="_blank" className="md:ml-12 flex justify-center">
+						<a
+							href="https://app.safulauncher.com/0xdca7905c38f15d5e653f76e956109bf5a4f43fbb"
+							target="_blank"
+							className="md:ml-12 flex justify-center"
+						>
 							<button className="safu-button-secondary">Buy Presale</button>
 						</a>
 					</div>
